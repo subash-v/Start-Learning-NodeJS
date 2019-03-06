c8b24f3f9391d117b6bea7a561e6a95cc98ff942
subash-v <subash.v@xelpmoc.in>
2019-03-06 13:47:07 +0530

    file server url module

---
+++ fileServer.js
@@ -0,0 +1,16 @@
+var http =require('http');
+var fs = require('fs');
+var url=require('url');
+http.createServer(function(req,res){
+    var q=url.parse(req.url,true);
+    var filename="."+q.pathname;
+    fs.readFile(filename,function(err,data){
+        if(err){
+            res.writeHead(404,{'Content-type':"text/html"})
+            return res.end("error:404 not found");
+        }
+        res.writeHead(200,{'Content-type':"text/html"})
+        res.write(data);
+        return res.end();
+    });
+}).listen(8080);
